df2ad287acba8616654508768dcb3023b6d699b9
Marcos Paulo Da Silva Erbas Junior <marcos.erbas@campos.local>
2024-06-01 16:07:22 -0300

    Implementação do Perceptron

---
+++ perceptron/perceptron_singleLayer.py
@@ -0,0 +1,54 @@
+# Montar peceptron com porta AND
+import matplotlib.pyplot as plt
+
+entradas = [[0, 0], [0, 1], [1, 0], [1, 1]]
+# Gabarito porta OR
+gabarito = [0, 1, 1, 1]
+
+# Gabarito porta AND
+#gabarito = [0, 0, 0, 1]
+
+peso1 = 0
+peso2 = 0
+pontos1 = []
+pontos2 = []
+taxaAprendizagem = 0.1 #Definida manualmente
+limiar = (0 + 1)/2 #Definição de limiar com base nas entradas possíveis
+
+while True:
+    n = 0
+    acerto = 0
+    for input in entradas:
+        x1 = input[0]
+        x2 = input[1]
+        output = x1 * peso1 + x2 * peso2
+
+        if output >= limiar:
+            output = 1
+        else:
+            output = 0
+
+        erro = gabarito[n] - output
+
+        if erro != 0:
+            pontos1.append(peso1)
+            pontos2.append(peso2)
+            peso1 = peso1 + (taxaAprendizagem * x1 * erro)
+            peso2 = peso2 + (taxaAprendizagem * x2 * erro)
+        else:
+            acerto += 1
+        n += 1
+    eficiencia = (acerto / len(entradas)) * 100
+    if eficiencia < 100:
+        print(f"A eficiência do Perceptron está em: {eficiencia}%")
+        print(f"Pesos: {peso1, peso2}")
+        print("__________________________________________________")
+    else:
+        x = pontos1
+        y = pontos2
+        plt.plot(x, y, 'ro')
+        plt.show()
+        print(f"A eficiência do Perceptron está em: {eficiencia}%")
+        print(f"Peso final: {peso1, peso2}")
+        print("__________________________________________________")
+        break
